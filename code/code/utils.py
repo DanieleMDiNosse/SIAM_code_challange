a842cc2d969008c1a6d17e3fd5219648540d67dd
fgt996 <fedefifa96@gmail.com>
2024-01-07 12:20:29 +0100

    Added target_4_opt

--- code/code/utils.py
+++ code/code/utils.py
@@ -240,6 +240,54 @@ def optimize_distribution(params, method='RockafellarUryasev'):
 
     return result.x
 
+def target_4_opt(theta, params, ret_inf=False, full_output=True):
+    '''
+    Target function for the optimization.
+    INPUT:
+        - theta: np.array containing the weights of the portfolio
+        - params: dict containing the parameters of the simulation
+        - ret_inf: bool, if True when the constraint P[ r>zeta ] > 0.7 is not
+            satisfied the output is np.inf; otherwise is np.nan
+        - full_output: bool, whenever to include also the constraint in the output
+    OUTPUT:
+        - cvar = If the constraint P[ r>zeta ] > 0.7 is satisfied, return CVaR.
+            Otherwise, the result is either np.nan or np.inf, according to ret_inf
+        - constraint = P[ r>zeta ] - 0.7
+    '''
+    np.random.seed(params['seed']) #Fix the seed for the next operations
+
+    #Initialize the pools
+    Rx0 = params['Rx0']
+    Ry0 = params['Ry0']
+    phi = params['phi']
+    pools = amm(Rx=Rx0, Ry=Ry0, phi=phi)
+
+    #The amount to invest on each pool is given by the weight in theta mult by the total capital
+    xs_0 = params['x_0']*theta
+    # Swap and mint
+    l = pools.swap_and_mint(xs_0)
+
+    # Simulate 1000 paths of trading in the pools
+    end_pools, Rx_t, Ry_t, v_t, event_type_t, event_direction_t =\
+        pools.simulate(
+            kappa=np.array(params['kappa']),
+            p=np.array(params['p']),
+            sigma=np.array(params['sigma']),
+            T=params['T'],
+            batch_size=params['batch_size'])
+    # Compute the log returns
+    log_ret = calculate_log_returns(xs_0, end_pools, l)
+
+    constraint= len(log_ret[ log_ret>params['zeta'] ]) / len(log_ret) - 0.7
+    if constraint >= 0:
+        cvar = calculate_cvar(log_ret) #If the constraint is satisfied, return CVaR
+    else: #Otherwise, return np.inf or np.nan
+        if ret_inf:
+            cvar = np.nan
+        else:
+            cvar = np.inf
+    return constraint, cvar
+
 def simulation_plots(x_0, params):
     """
     Plots the evolution of the reserves, the price and the returns for a given
